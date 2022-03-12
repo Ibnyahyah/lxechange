4290989cdb8f4c1e18fe5f8879ea857a7e9fa90e
ibn2085 <yahyahridwan665@gmail.com>
2022-03-12 15:49:40 +0545

    comminting

--- src/components/transactions/transaction.js
+++ src/components/transactions/transaction.js
@@ -34,24 +34,27 @@ function Transaction(){
     return(
         <div className="trade-transaction">
             <table style={{width:'100%'}}>
+            {transactions <= 0?<p className="font-3 text-center text-black">Oopps No Transaction Record Found!!! Kindly Trade</p>:  
+            <>
                 <thead className="bg-black text-white">
-                    <tr>
-                        <th>S/N</th>
-                        <th>Type</th>
-                        <th>Amount</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-                {transactions.map((data, index)=>(
-                        <tbody>
-                            <tr key={data.TotalAmountInNaira}>
-                                <td>NAN</td>
-                                <td>{data.TradeType}</td>
-                                <td>&#8358;{data.TotalAmountInNaira}</td>
-                                <td>{(new Date(data.Date).toTimeString())}</td>
-                            </tr>
-                        </tbody>
-                    ))}
+                        <tr>
+                            <th>S/N</th>
+                            <th>Type</th>
+                            <th>Amount</th>
+                            <th>Data</th>
+                        </tr>
+                    </thead>
+                        {transactions.map((data, index)=>(
+                            <tbody>
+                                <tr key={data.TotalAmountInNaira}>
+                                    <td>NAN</td>
+                                    <td>{data.TradeType}</td>
+                                    <td>&#8358;{data.TotalAmountInNaira}</td>
+                                    <td>{(new Date(data.Date).toTimeString())}</td>
+                                </tr>
+                            </tbody>
+                        ))}
+                </>}
             </table>
         </div>
     )
